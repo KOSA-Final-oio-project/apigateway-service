20a483a9f7acc08df41817f2eb4e90341d23efae
hakyoon <khakyy@naver.com>
2023-12-20 18:17:22 +0900

    feat: Spring Cloud Config Bootstrap 추가

--- src/main/java/com/oio/apigatewayservice/ApigatewayServiceApplication.java
+++ src/main/java/com/oio/apigatewayservice/ApigatewayServiceApplication.java
@@ -1,13 +1,16 @@
 package com.oio.apigatewayservice;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class ApigatewayServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApigatewayServiceApplication.class, args);
+		new SpringApplicationBuilder(ApigatewayServiceApplication.class)
+				.web(WebApplicationType.REACTIVE) // .REACTIVE, .SERVLET
+				.run(args);
 	}
 
 }
